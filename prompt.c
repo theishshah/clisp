48679e999fff57ca95048fec249372ac36954ee7
Ish Shah <ishshah2@illinois.edu>
2017-02-10 14:34:28 -0600

    testing prompt

---
+++ prompt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+static char input[2048];
+
+int main(int argc, char** argv) {
+    
+    puts("clisp");
+    puts("ctrl+c to exit");
+
+    while(1){
+    
+        fputs("clisp> ". stdout);
+
+        fgets(input, 2048, stdin);
+
+        printf("No you're a %s". input);
+    }
+
+return 0;
+}
